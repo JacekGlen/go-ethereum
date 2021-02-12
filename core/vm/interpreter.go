2ab324298dcf322747210858ca89580be2bcb87a
pdobacz <5735525+pdobacz@users.noreply.github.com>
2021-02-12 19:02:16 +0100

    exclude some stuff from Tracer.CaptureState

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -278,17 +278,17 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 			mem.Resize(memorySize)
 		}
 
-		if in.cfg.Debug {
-			in.cfg.Tracer.CaptureState(in.evm, pc, op, gasCopy, cost, mem, stack, returns, in.returnData, contract, in.evm.depth, err)
-			logged = true
-		}
-
 		// execute the operation
 		res, err = operation.execute(&pc, in, callContext)
 		// if the operation clears the return data (e.g. it has returning data)
 		// set the last return to the result of the operation.
 		if operation.returns {
 			in.returnData = common.CopyBytes(res)
+		}
+
+		if in.cfg.Debug {
+			in.cfg.Tracer.CaptureState(in.evm, pc, op, gasCopy, cost, mem, stack, returns, in.returnData, contract, in.evm.depth, err)
+			logged = true
 		}
 
 		switch {
